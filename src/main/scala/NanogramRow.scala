9e9de3990484787bdbf6dcae9a568e1b10abd898
Daniel <d.fiedosiuk@adform.com>
2022-04-01 10:25:58 +0200

    Created Nanogram.check(). All tests passed

--- src/main/scala/NanogramRow.scala
+++ src/main/scala/NanogramRow.scala
@@ -1,6 +1,11 @@
 object NanogramRow extends App {
 
-  def countOnes(row: Array[Int]):  Array[Int] ={
-    ???
+  def countOnes(row: Array[Int])={
+    row
+      .foldLeft("")(_+_)
+      .split("0+")
+      .map(_.length)
+      .filterNot(_==0)
   }
+
 }
--- src/test/scala/NanogramRowSpec.scala
+++ src/test/scala/NanogramRowSpec.scala
@@ -13,12 +13,12 @@ class NanogramRowSpec extends AnyFlatSpec with should.Matchers {
     "the lengths of the sets of consecutive 1's in the input array, " +
     "in order from left to right " in {
     countOnes(Array.empty) shouldBe Array.empty
-//    countOnes(Array[0,0,0,0,0]) shouldBe Array.empty
-//    countOnes(Array[1,1,1,1,1]) shouldBe Array[5]
-//    countOnes(Array[0,1,1,1,1,1,0,1,1,1,1]) shouldBe Array[5,4]
-//    countOnes(Array[1,1,0,1,0,0,1,1,1,0,0]) shouldBe Array[2,1,3]
-//    countOnes(Array[0,0,0,0,1,1,0,0,1,0,1,1,1]) shouldBe Array[2,1,3]
-//    countOnes(Array[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]) shouldBe Array[1,1,1,1,1,1,1,1]
+    countOnes(Array(0,0,0,0,0)) shouldBe Array.empty
+    countOnes(Array(1,1,1,1,1)) shouldBe Array(5)
+    countOnes(Array(0,1,1,1,1,1,0,1,1,1,1)) shouldBe Array(5,4)
+    countOnes(Array(1,1,0,1,0,0,1,1,1,0,0)) shouldBe Array(2,1,3)
+    countOnes(Array(0,0,0,0,1,1,0,0,1,0,1,1,1)) shouldBe Array(2,1,3)
+    countOnes(Array(1,0,1,0,1,0,1,0,1,0,1,0,1,0,1)) shouldBe Array(1,1,1,1,1,1,1,1)
   }
 
 }
